13ace68adcdc4cc0ef38afb50537690ef5da28f1
Adam Schwartz <aschwartz@hubspot.com>
2014-07-22 14:12:49 -0400

    Initial commit

---
+++ eager-disqus.js
@@ -0,0 +1,15 @@
+window.EagerDisqus = {
+  init: function(element, options) {
+    element.innerHTML = '' +
+      '<div id="disqus_thread"></div>' +
+      '<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>';
+
+    window.disqus_shortname = options.disqusShortName;
+
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//' + options.disqusShortName + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  }
+};
